ffcdea7b073705abbb08f2d0122c17bfb16b2725
Alejandro Guerrero <43582291+Alejgl04@users.noreply.github.com>
2023-06-19 19:23:34 -0400

    button add register

---
+++ src/users/presentation/render-add-button/render-add-button.js
@@ -0,0 +1,20 @@
+
+import './render-add-button.css';
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderAddButton = ( element ) => {
+
+  const addButton = document.createElement('button');
+  addButton.innerText = '+';
+  addButton.classList.add('add-button');
+
+  element.append( addButton );
+
+  addButton.addEventListener('click', () => {
+    throw Error('Error');
+  })
+
+}
